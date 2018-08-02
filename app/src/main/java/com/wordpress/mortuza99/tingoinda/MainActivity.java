2c564ece568a2a9f836b17e29f776b880f3d03bd
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-02 20:04:51 +0600

    Initial Commit

---
+++ app/src/main/java/com/wordpress/mortuza99/tingoinda/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wordpress.mortuza99.tingoinda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
