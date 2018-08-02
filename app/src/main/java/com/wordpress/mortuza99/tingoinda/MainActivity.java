cf1a6a42e3ebe03635e344d682aaa971fd2c6336
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-02 21:11:38 +0600

    Done Adding Fragment And Bottom Navigation Drawer.

--- app/src/main/java/com/wordpress/mortuza99/tingoinda/MainActivity.java
+++ app/src/main/java/com/wordpress/mortuza99/tingoinda/MainActivity.java
@@ -1,13 +1,64 @@
 package com.wordpress.mortuza99.tingoinda;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.wordpress.mortuza99.tingoinda.Fragments.Home;
+import com.wordpress.mortuza99.tingoinda.Fragments.Library;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mNavigationView;
+    private FrameLayout mMaineFrame;
+
+    private Home homeFragment;
+    private Library libraryFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMaineFrame = findViewById(R.id.mainframe);
+        mNavigationView = findViewById(R.id.navbottom);
+
+        homeFragment = new Home();
+        libraryFragment = new Library();
+
+        setFragment(homeFragment);
+
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.navhome:
+                        Toast.makeText(getApplicationContext(), "HOME", Toast.LENGTH_LONG).show();
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.navlibrary:
+                        Toast.makeText(getApplicationContext(), "LIBRARY", Toast.LENGTH_LONG).show();
+                        setFragment(libraryFragment);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainframe, fragment);
+        fragmentTransaction.commit();
     }
 }
